0c8283b21547914504994f28f19d8c6057945efe
Lfillon <lucas_fillon@hotmail.fr>
2017-04-25 16:55:03 +0200

    Add files via upload

--- Sources/Network.cpp
+++ Sources/Network.cpp
@@ -1,4 +1,4 @@
-Network(size_t S_data_in, size_t N_input = 1, size_t N_hide = 1, size_t N_output = 1, TYPE_NEURON type = STEP, double learning_rate_def = 0.5 ){
+Layer::Network(size_t S_data_in, size_t N_input = 1, size_t N_hide = 1, size_t N_output = 1, TYPE_NEURON type = STEP, double learning_rate_def = 0.5 ){
 
   Layer In = Layer(input,N_input , type, learning_rate_def);
   Layer out = Layer(N_input, N_hide , type, learning_rate_def);
@@ -7,18 +7,35 @@ Network(size_t S_data_in, size_t N_input = 1, size_t N_hide = 1, size_t N_output
 
 }
 
-bool train(Data input, Data output){
+bool Layer::train(Data input, Data output){
+
+  In.input = input; // give data to the In-layer
+  In.update_output();// calculate all output of the In-layer
+
+  Hide.input << In;  // give outputs of In-layer to the Hide-layer
+  Hide.update_output();// calculate all output of the Hide-Layer
+
+  Out.input << Hide; // give outputs of Hide-layer to the out-layer
+  Out.update_output();// calculate all output of the Out-layer
+
+  vector<double> errors_for_out;
+
+  for (int i = 0; i <= neurons_tab.size() ; i++) {
+    errors_for_out.push_back( Out.output[i] - output.data[i] );
+  }
+
+  Out.update_layer(errors_for_out);
+  Hide.update_layer(Out.get_errors());
+}
+
+string Layer::detect(Data input){
 
 }
 
-string detect(Data input){
+void Layer::get_max_prob(double *prob, string *output){
 
 }
 
-void get_max_prob(double *prob, string *output){
-
-}
-
-double get_prob(string out){
+double Layer::get_prob(string out){
 
 }
